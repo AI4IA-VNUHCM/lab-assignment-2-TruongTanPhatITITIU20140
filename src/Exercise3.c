4e39307ebd27c2286bd63c92453961e29e0c232c
TruongTanPhat <ITITIU20140@student.hcmiu.edu.vn>
2021-10-11 15:37:30 +0700

    exe 3

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -12,10 +12,29 @@ ______________________________________
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
-	int testcase = atoi(argv[1]);
-	
+	int testcase = atoi(argv[1]);	
 	//Your codes here
-	
+	if(testcase ==1) return 0;
+	// loop while chia cho 2 đến khi k chia được nữa (in 2 mỗi lần chạy)
+	while(testcase % 2 == 0)
+	{ 
+		printf(" %d *",2);
+		testcase = testcase /2;
+	}
+	// loop for chia cho số từ 3 tới căn n (đk check số prime)
+	 for (int i = 3; i <= sqrt(testcase); i = i+2)
+    {
+        // While i divides n, print i and divide n
+		// in ra số prime chia hết cho n
+        while (testcase % i == 0)
+        {
+            printf(" %d", i);
+            testcase = testcase / i;
+        }
+    }
+	//neu n la so prime lon hon 2
+	if (testcase > 2)
+        printf (" %d", testcase);
 	
 	return 0;
 }
